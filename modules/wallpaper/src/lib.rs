8516bd70474c3921d94b7ea84fbda39610808cd6
magyx <w.magyx@gmail.com>
2026-04-11 23:08:25 +0200

    fix: clock should be fit length

--- modules/wallpaper/src/lib.rs
+++ modules/wallpaper/src/lib.rs
@@ -293,7 +293,9 @@ impl OrbitModule for Wallpaper {
                     time_format,
                 } => {
                     let time = chrono::Local::now().format(time_format).to_string();
-                    let mut text = Text::new(time, *font_size).family(Family::Monospace);
+                    let mut text = Text::new(time, *font_size)
+                        .family(Family::Monospace)
+                        .size(Size::splat(Length::Fit));
                     if let Some(family) = font_family {
                         text = text.family(family.clone().into());
                     }
